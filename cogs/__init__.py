f2824421d479a69ee4a5a05ab4daf0f1221bcf8c
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-07-25 08:36:02 +0900

    Update __init__.py

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,15 +1,1 @@
-import discord
-from discord.ext import commands
-
-class CreateGroup(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        print("[CreateGroup] Cog initialized.")
-
-    async def cog_load(self):
-        print("[CreateGroup] cog_load started.")
-        await self.load_persistent_views()
-        print("[CreateGroup] persistent views loaded.")
-
-    # load_persistent_viewsはそのまま残す
 
