b3f4f74bf5c822814a45b8945831db3b3c031bc6
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-07-25 08:40:06 +0900

    Update restart.py

--- cogs/restart.py
+++ cogs/restart.py
@@ -2,17 +2,17 @@ import os
 import sys
 from discord.ext import commands
 
-class ControlCog(commands.Cog):
+class RestartCog(commands.Cog):  # Cog名を変更
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(name="restart")
     @commands.is_owner()
-    async def restart(self, ctx):
+    async def restart_bot(self, ctx):
         await ctx.send("Botを再起動します...")
         await self.bot.close()
         # プロセスを自身の実行ファイルで再起動
         os.execv(sys.executable, [sys.executable] + sys.argv)
 
 async def setup(bot):
-    await bot.add_cog(ControlCog(bot))
+    await bot.add_cog(RestartCog(bot))
