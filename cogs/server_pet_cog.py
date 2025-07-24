a2cabf48f93f79207696f9a3af38072400c4f7d9
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-07-24 09:30:07 +0900

    Update server_pet_cog.py

--- cogs/server_pet_cog.py
+++ cogs/server_pet_cog.py
@@ -1,3 +1,7 @@
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 @commands.command()
 async def test(ctx):
     await ctx.send("Botコマンドは動いています！")
