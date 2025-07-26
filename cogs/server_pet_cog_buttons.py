ae3cc7a31c6c184233cc673f162153285e1f4121
mimic469 <black.heart829@gmail.com>
2025-07-26 11:43:33 +0900

    Update server_pet_cog_buttons.py

--- cogs/server_pet_cog_buttons.py
+++ cogs/server_pet_cog_buttons.py
@@ -41,7 +41,7 @@ class PetGame(commands.Cog):
 
 #mimic test
 class PetView(View):
-    PET_IMAGE_URL = "https://github.com/kuzyoumike0/Mikezerobot/blob/main/images/pet_fuwafuwa_happy.png"
+    PET_IMAGE_URL = "https://raw.githubusercontent.com/kuzyoumike0/Mikezerobot/main/images/pet_fuwafuwa_happy.png"
     #ペットイメージのURL指定
 
     def __init__(self, bot, user):
